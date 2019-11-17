4eb49258d8f2e1ebdf65932bfa0783acb1bb6a44
Michael Padiernos <mike.padiernos@gmail.com>
2019-11-17 09:50:36 -0500

    WORK: fixed number 0 width

--- src/components/styles.js
+++ src/components/styles.js
@@ -3,11 +3,11 @@ import {csx, css} from "emotion";
 
 export const btn = css`
 		border-radius: 50px;
-		font-size: 1.5rem;
+		font-size: 2rem;
 		width: 100px;
 		height: 100px;
 		&:nth-child(10) {
-      width: 200px;
+      width: 235px;
       grid-column: span 2;
     }
 	`;
